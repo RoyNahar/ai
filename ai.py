434ec29c71e0e013d9a03944c4f46fe7dc4ff98d
Krashfire <34046594+RoyNahar@users.noreply.github.com>
2023-03-13 16:28:16 +0000

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,33 +1,49 @@
 import requests
 import os
+import json
 
 
-#Define function to download required libraries for AI self-learning
+# Define function to download required libraries for AI self-learning
 def download_libraries():
-libraries = ['numpy', 'pandas', 'scikit-learn', 'tensorflow']
-for library in libraries:
-os.system('pip install ' + library)
+    libraries = ['numpy', 'pandas', 'scikit-learn', 'tensorflow']
+    for library in libraries:
+        os.system('pip install ' + library)
 
-#Define function to train AI on user-provided data
+
+# Define function to train AI on user-provided data
 def train_AI(data):
+    # insert your AI training code here
+    pass
 
-# insert your AI training code here
-pass
 
-#Define main function to read user instructions and perform appropriate action
+# Define function to fetch data from URL
+def fetch_data(url):
+    response = requests.get(url)
+    data = json.loads(response.text)
+    # process and store data as needed
+
+
+# Define main function to read user instructions and perform appropriate action
 def main():
-while True:
-instruction = input("Please enter your instruction: ")
-if instruction == "train AI":
-data = input("Please enter your data: ")
-train_AI(data)
-elif instruction == "download libraries":
-download_libraries()
-print("Libraries downloaded successfully!")
-elif instruction == "stop":
-break
-else:
-print("Invalid instruction. Please try again.")
+    while True:
+        instruction = input("Please enter your instruction: ")
+        if instruction == "train AI":
+            data = input("Please enter your data: ")
+            train_AI(data)
+        elif instruction == "download libraries":
+            download_libraries()
+            print("Libraries downloaded successfully!")
+        elif instruction.startswith("fetch data"):
+            url = instruction.split(" ")[-1]
+            fetch_data(url)
+            print("Data fetched successfully!")
+        elif instruction == "stop":
+            break
+        else:
+            print("Invalid instruction. Please try again.")
 
-if name == 'main':
-main()
+
+if __name__ == '__main__':
+    main()
+
+
